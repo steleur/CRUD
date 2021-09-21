82bf79db0aa1fc471fd3075e713d6fd793665739
= <k.dratsevich@gmail.com>
2021-09-21 18:04:48 +0300

    initial

---
+++ src/main/java/by/dratsevich/mycrud/users/UserService.java
@@ -0,0 +1,37 @@
+package by.dratsevich.mycrud.users;
+
+import java.util.List;
+import java.util.Optional;
+import javax.persistence.Id;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+  @Autowired private UserRepository repo;
+
+  public List<User> listAll() {
+    return (List<User>) repo.findAll();
+  }
+
+  public void save(User user) {
+    repo.save(user);
+  }
+
+  public User get (Integer id) throws UserNotFoundExeption {
+    Optional<User> result = repo.findById(id);
+    if (result.isPresent()) {
+      return result.get();
+    }
+    throw new UserNotFoundExeption("Could not find any user with ID " + id);
+  }
+
+  public void delelete(Integer id) throws UserNotFoundExeption {
+   Long count = repo.countById(id);
+   if (count == null || count == (long) 0) {
+     throw new UserNotFoundExeption("Could not find any user with id " + id);
+   }
+    repo.deleteById(id);
+
+  }
+}
